65e6cca3c962aa47a6994544a0a01082f4d086e8
ilanlavi <refaelberlin@gmail.com>
2022-04-29 18:39:55 +0200

    initial setup

--- routes/base.js
+++ routes/base.js
@@ -5,7 +5,7 @@ const router = express.Router();
 const routeGuard = require('./../middleware/route-guard');
 
 router.get('/', (req, res, next) => {
-  res.render('home', { title: 'Hello World!' });
+  res.render('home', { title: 'BookBudd!' });
 });
 
 router.get('/private', routeGuard, (req, res, next) => {
