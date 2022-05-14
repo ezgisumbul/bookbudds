7a0aa0b0891d6abb3f4a45100981f50cca7186bb
ilanlavi <refaelberlin@gmail.com>
2022-05-14 13:32:06 +0200

    last update

--- middleware/checkBook.js
+++ middleware/checkBook.js
@@ -17,31 +17,39 @@ const checkBook = (req, res, next) => {
   res.locals.isLogged = isLogged;
   console.log(isLogged)
 
-  // CHECK if Book exists in the Books collection
-  Book.findOne({ bookId: bookId })
-    .then((book) => {
-      if (isLogged) {
-        // CHECK multiple conditions on FindOne: user-id + the book +id
-        User.findOne({ _id: req.user._id, books: book._id })
-          .then((user) => {
-            // If we get a user document,
-            // that means we have a user in the database
-            // with our user id and the book id in that same document
-            if (user) {
-              bookCheck = true;
-            } else {
-              bookCheck = false;
-            }
-            res.locals.bookCheck = bookCheck;
-            next();
-          })
-      } else {
-        console.log("The user is not logged")
-        next();
-      }
-    })
-    .catch(error => {
-      next(error);
-    })
+  if (isLogged) {
+    // CHECK if Book exists in the Books collection
+    Book.findOne({ bookId: bookId })
+      .then((book) => {
+        if (book) {
+          // CHECK multiple conditions on FindOne: user-id + the book +id
+          User.findOne({ _id: req.user._id, books: book._id })
+            .then((user) => {
+              // If we get a user document,
+              // that means we have a user in the database
+              // with our user id and the book id in that same document
+              if (user) {
+                bookCheck = true;
+              } else {
+                bookCheck = false;
+              }
+              res.locals.bookCheck = bookCheck;
+              next();
+            })
+            .catch(error => {
+              next(error);
+            })
+        } else {
+          next();
+        }
+      })
+      .catch(error => {
+        next(error);
+      })
+  } else {
+    console.log("The user is not logged")
+    next();
+  }
+
 }
 module.exports = checkBook;
